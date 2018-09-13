08e0e01ac5b590ecdcb889b53c3e5e763ef8f71a
Stefan Sohlstrom <stefan.sohlstrom@uber.com>
2018-09-13 09:29:25 -0700

    new team members

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-const eligbleTeam = ['Tomo', 'Enoch', 'Arkady', 'Jack', 'Jake', 'Mihai']
+const eligbleTeam = ['Tomo', 'Enoch', 'Arkady', 'Jack', 'Jake', 'Mihai', 'Michael', 'Anh', 'Yan', 'Mark']
 
 exec(`cowsay ${eligbleTeam[Math.round(Math.random() * (eligbleTeam.length))] + ' is Scrum Master!'}`, (e, stdout, err) => {
   console.log(stdout);
